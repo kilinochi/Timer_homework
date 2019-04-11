cd8d502e1c73ab34cdc16252d3090315a6f384e4
arman-sh <rosko1245@gmail.com>
2019-04-11 20:52:46 +0300

    v 1.5 - correct Timer

--- app/src/main/java/ru/ok/timer/MainActivity.java
+++ app/src/main/java/ru/ok/timer/MainActivity.java
@@ -8,6 +8,7 @@ import ru.ok.timer.services.TimerService;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -19,7 +20,12 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final long START_TIME_IN_MILLIS = 600000;
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private static final String SHARED_PREFERENCE_TAG = "TIMER_PREFERENCE";
+    private static final String TIME_MILLIS_LEFT_TAG = "TIME_MILLIS_LEFT";
+    private static final String TIMER_RUNNING_TAG = "TIMER_RUNNING";
+    private static final String END_TIMER_TAG = "END_TIMER";
+    private long mTimeLeftInMillis;
+    private long endTimer;
     private TextView timerView;
     private static final String TIMER_VALUE_TAG = "TIMER_VALUE";
     private CountDownTimer countDownTimer;
@@ -27,6 +33,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button startStopButton;
     private Button resetButton;
     private Intent timerIntent;
+
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_TAG, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong(TIME_MILLIS_LEFT_TAG, mTimeLeftInMillis);
+        editor.putLong(END_TIMER_TAG, endTimer);
+        editor.putBoolean(TIMER_RUNNING_TAG, timerRunning);
+        editor.apply();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFERENCE_TAG, MODE_PRIVATE);
+        mTimeLeftInMillis = sharedPreferences.getLong(TIME_MILLIS_LEFT_TAG, START_TIME_IN_MILLIS);
+        timerRunning = sharedPreferences.getBoolean(TIMER_RUNNING_TAG, false);
+        updateTimer();
+        updateButtons();
+        if(timerRunning) {
+            endTimer = sharedPreferences.getLong(END_TIMER_TAG, 0);
+            mTimeLeftInMillis = endTimer - System.currentTimeMillis();
+            if(mTimeLeftInMillis < 0) {
+                mTimeLeftInMillis = 0;
+                timerRunning = false;
+                updateTimer();
+                updateButtons();
+            }
+            else {
+                startTimer();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +82,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resetButton.setOnClickListener(this);
     }
 
-    public void startTimerService() {
+    private void startTimerService() {
         String timerValue = timerView.getText().toString();
         timerIntent.putExtra(TIMER_VALUE_TAG, timerValue);
         ContextCompat.startForegroundService(this, timerIntent);
     }
 
-    public void stopTimerService() {
+    private void stopTimerService() {
         stopService(timerIntent);
     }
 
@@ -71,10 +113,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void stopTimer() {
         countDownTimer.cancel();
         timerRunning = false;
-        startStopButton.setText(R.string.button_start);
+        updateButtons();
     }
 
     private void startTimer() {
+        endTimer = System.currentTimeMillis() + mTimeLeftInMillis;
         countDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -85,11 +128,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onFinish() {
                 timerRunning = false;
-                startStopButton.setText(getString(R.string.button_start));
+                updateButtons();
             }
         }.start();
         timerRunning = true;
-        startStopButton.setText(getString(R.string.timer_pause));
+        updateButtons();
     }
 
     private void updateTimer() {
@@ -97,10 +140,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         timerView.setText(timeLeftFormatted);
+        if(timerRunning) {
+            startTimerService();
+        }
+        else {
+            stopTimerService();
+        }
     }
 
     private void resetTimer() {
-        mTimeLeftInMillis = START_TIME_IN_MILLIS;;
+        timerRunning = false;
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
         updateTimer();
+    }
+
+    private void updateButtons() {
+        if(timerRunning) {
+            startStopButton.setText(R.string.timer_pause);
+            resetButton.setVisibility(View.GONE);
+        }
+        else {
+            startStopButton.setText(R.string.button_start);
+            if(mTimeLeftInMillis < 1000) {
+                startStopButton.setVisibility(View.GONE);
+            }
+            else {
+                startStopButton.setVisibility(View.VISIBLE);
+            }
+            if(mTimeLeftInMillis < START_TIME_IN_MILLIS) {
+                resetButton.setVisibility(View.VISIBLE);
+            }
+            else {
+                resetButton.setVisibility(View.GONE);
+            }
+        }
     }
 }
